e2e0b73cc7c622d1e5e7c1321cb72fcb3c270e83
Antonio Pierro <antonio.pierro@gmail.com>
2014-11-03 10:10:30 +0100

    Fix the indentation

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,74 +1,33 @@
-angular.module('starter.controllers', [])
+(function() {
+    'use strict';
 
-.controller('DashCtrl', function($scope) {
-})
+    angular.module('starter.controllers', [])
+        .controller('DashCtrl', function($scope) {})
+        .controller('DestinationsCtrl', function($scope, Destinations) {
+            $scope.destinations = Destinations.all();
+        })
+        .controller('AccountCtrl', function($scope) {})
+        .controller('MapCtrl', function($scope, $ionicLoading, $compile, $stateParams, Destinations) {
+            $scope.destination = Destinations.get($stateParams.destinationId);
+            var initialize =  function () {
+                var myLatlng = new google.maps.LatLng($scope.destination.lat, $scope.destination.lng);
+                var mapOptions = {
+                    center: myLatlng,
+                    zoom: 19,
+                    mapTypeId: google.maps.MapTypeId.SATELLITE
+                };
+                var map = new google.maps.Map(document.getElementById("map"),
+                    mapOptions);
+                var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    map: map,
+                    title: 'Uluru (Ayers Rock)'
+                });
+                $scope.map = map;
+            }
 
-.controller('DestinationsCtrl', function($scope, Destinations) {
-  $scope.destinations = Destinations.all();
-})
-
-.controller('AccountCtrl', function($scope) {
-})
-
-.controller('MapCtrl', function($scope, $ionicLoading, $compile, $stateParams, Destinations) {
-
-  $scope.destination = Destinations.get($stateParams.destinationId);
-
-  function initialize() {
-    var myLatlng = new google.maps.LatLng($scope.destination.lat, $scope.destination.lng);
-
-    var mapOptions = {
-      center: myLatlng,
-      zoom: 16,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map"),
-        mapOptions);
-
-    //Marker + infowindow + angularjs compiled ng-click
-    var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-    var compiled = $compile(contentString)($scope);
-
-    var infowindow = new google.maps.InfoWindow({
-      content: compiled[0]
-    });
-
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Uluru (Ayers Rock)'
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(map,marker);
-    });
-
-    $scope.map = map;
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
-
-  initialize();
-
-  $scope.centerOnMe = function() {
-    if(!$scope.map) {
-      return;
-    }
-
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function(error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
-
-  $scope.clickTest = function() {
-    alert('Example of infowindow with ng-click')
-  };
-
-});
+            // TODO - fix the call of initialize
+            google.maps.event.addDomListener(window, 'load', initialize);
+            initialize();
+        });
+})();
--- www/js/services.js
+++ www/js/services.js
@@ -1,27 +1,46 @@
-angular.module('starter.services', [])
+(function() {
+    'use strict';
 
-/**
- * A simple example service that returns some data.
- */
-.factory('Destinations', function() {
-  // Might use a resource here that returns a JSON array
+    angular.module('starter.services', [])
+        /**
+         * A simple example service that returns some data.
+         */
+        .factory('Destinations', function() {
+            // Might use a resource here that returns a JSON array
+            // Some fake testing data
+            var destinations = [
+                // TODO - Update the right values of lat and lng for each name
+                {
+                    id: 0,
+                    name: 'Polo Umanistico',
+                    lat: 39.228842,
+                    lng: 9.111931
+                }, {
+                    id: 1,
+                    name: 'Polo Scienze Sociali',
+                    lat: 39.228842,
+                    lng: 9.111931
+                }, {
+                    id: 2,
+                    name: 'Polo Ingegneria e Architettura',
+                    lat: 39.228842,
+                    lng: 9.111931
+                }, {
+                    id: 3,
+                    name: 'Polo Scientifico',
+                    lat: 39.228842,
+                    lng: 9.111931
+                }
+            ];
 
-  // Some fake testing data
-  var destinations = [
-    // TODO - Update the right values of lat and lng for each name
-    { id: 0, name: 'Polo Umanistico', lat: 39.2292587, lng: 9.1109665},
-    { id: 1, name: 'Polo Scienze Sociali', lat: 39.2292587, lng: 9.1109665},
-    { id: 2, name: 'Polo Ingegneria e Architettura', lat: 39.2292587, lng: 9.1109665},
-    { id: 3, name: 'Polo Scientifico', lat: 39.2292587, lng: 9.1109665}
-  ];
-
-  return {
-    all: function() {
-      return destinations;
-    },
-    get: function(destinationId) {
-      // Simple index lookup
-      return destinations[destinationId];
-    }
-  }
-});
+            return {
+                all: function() {
+                    return destinations;
+                },
+                get: function(destinationId) {
+                    // Simple index lookup
+                    return destinations[destinationId];
+                }
+            }
+        });
+})();
