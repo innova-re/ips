2a871fc1e6a35861efbf9383ffb66470297259ac
Antonio Pierro <antonio.pierro@gmail.com>
2014-12-05 12:48:40 +0100

    Updated the latitude and longitude coordinates

--- www/js/services/destination.service.js
+++ www/js/services/destination.service.js
@@ -13,18 +13,18 @@
             }, {
                 id: 1,
                 name: 'Polo Scienze Sociali',
-                lat: 39.228842,
-                lng: 9.111931
+                lat: 39.223481,
+                lng: 9.111309
             }, {
                 id: 2,
                 name: 'Polo Ingegneria e Architettura',
-                lat: 39.228842,
-                lng: 9.111931
+                lat: 39.230142,
+                lng: 9.108063
             }, {
                 id: 3,
                 name: 'Polo Scientifico',
-                lat: 39.228842,
-                lng: 9.111931
+                lat: 39.222339,
+                lng: 9.114032
             }
         ];
 
