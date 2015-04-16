0941c0ab14297c533d276fcb7a53a5c3d6805624
Antonio Pierro <antonio.pierro@gmail.com>
2015-04-16 12:29:14 +0200

    Added the overlay image into map-web.controller

--- www/js/controllers/map-controllers/map-web.controller.js
+++ www/js/controllers/map-controllers/map-web.controller.js
@@ -4,11 +4,18 @@
     define([], function () {
 
         return function ($scope, div) {
-            var latLng,
+
+            var imageBounds,
+                latLng,
                 mapOptions,
                 map,
-                marker;
+                marker,
+                overlay;
 
+            imageBounds = new google.maps.LatLngBounds(
+                  new google.maps.LatLng(39.270148, 9.123533),
+                  new google.maps.LatLng(39.270630, 9.124764)
+            );
             latLng = new google.maps.LatLng($scope.laboratory.lat, $scope.laboratory.lng);
             mapOptions = {
                 center: latLng,
@@ -20,6 +27,11 @@
                 position: latLng,
                 map: map
             });
+            overlay = new google.maps.GroundOverlay(
+                'images/monserrato-blocco-G-0.png',
+                imageBounds
+            );
+            overlay.setMap(map);
         };
     });
 }(this.define));
