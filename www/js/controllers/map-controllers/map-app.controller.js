3c45ee5e343dfd73532624e000df06da14b435d0
Antonio Pierro <antonio.pierro@gmail.com>
2015-04-17 11:52:49 +0200

    Refactored the map-controller

--- www/js/controllers/map-controllers/map-app.controller.js
+++ www/js/controllers/map-controllers/map-app.controller.js
@@ -1,22 +1,68 @@
-(function (define, plugin) {
+(function (document, define, plugin, launchnavigator) {
     'use strict';
 
     define([
-        '../../services/map-options.service',
-        '../../factories/on-map-init.factory'
-    ], function (mapOptionsService, onMapInitFactory) {
+        '../../services/map-options.service'
+    ], function (mapOptionsService) {
 
         return function ($scope, div) {
 
-            var latLng,
-                mapOptions,
-                map;
+            var mapOptions,
+                map,
+                onMapReady;
 
-            latLng = new plugin.google.maps.LatLng($scope.laboratory.lat, $scope.laboratory.lng);
-            mapOptions = mapOptionsService(latLng);
+            mapOptions = mapOptionsService($scope.laboratory.lat, $scope.laboratory.lng);
             map = plugin.google.maps.Map.getMap(div, mapOptions);
+            onMapReady = function () {
 
-            map.on(plugin.google.maps.event.MAP_READY, onMapInitFactory);
+                // Add get-direction button functionality
+                var getDirectionButton,
+                    MapBase,
+                    mapBase,
+                    onGetDirection;
+
+                getDirectionButton = document.getElementsByClassName('map-get-direction')[0];
+
+                onGetDirection = function () {
+                    launchnavigator.navigate(
+                        [$scope.laboratory.lat, $scope.laboratory.lng],
+                        null,
+                        function () {
+                            return true;
+                        },
+                        function (error) {
+                            alert.log('Plugin error: ' + error);
+                        }
+                    );
+                };
+                MapBase = function () {
+                    plugin.google.maps.BaseClass.apply(this);
+                };
+                MapBase.prototype = new plugin.google.maps.BaseClass();
+                mapBase = new MapBase();
+                mapBase.on('getDirectionEvent', onGetDirection);
+                getDirectionButton.addEventListener('click', function () {
+                    mapBase.trigger('getDirectionEvent');
+                });
+
+                // Add a GroundOverlay
+                var bounds = [
+                    new plugin.google.maps.LatLng(39.270148, 9.123533),
+                    new plugin.google.maps.LatLng(39.270630, 9.124764)
+                ];
+                map.addGroundOverlay({
+                    url: 'www/images/monserrato-blocco-G-0.png',
+                    bounds: bounds,
+                    opacity: 0.8
+                }, function (groundOverlay) {
+                    map.showDialog();
+                    map.animateCamera({
+                        'target': bounds
+                    });
+                });
+            };
+            map.on(plugin.google.maps.event.MAP_READY, onMapReady);
+
         };
     });
-}(this.define, this.plugin));
+}(this.document, this.define, this.plugin, this.launchnavigator));
