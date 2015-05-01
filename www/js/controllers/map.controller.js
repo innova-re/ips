11cf1213b3d56a2f2564a38a0ce2550987f3afa9
Antonio Pierro <antonio.pierro@gmail.com>
2015-05-01 12:06:31 +0200

    Added geojson data layer

--- www/js/controllers/map.controller.js
+++ www/js/controllers/map.controller.js
@@ -2,8 +2,9 @@
     'use strict';
 
     define([
-        '../services/laboratories.service'
-    ], function (laboratoriesService) {
+        '../services/laboratories.service',
+        'text!../../../geojson/cittadella-universitaria.geojson.json'
+    ], function (laboratoriesService, dataGeoJson) {
 
         return ['$scope', '$stateParams', '$http', function ($scope, $stateParams, $http) {
 
@@ -14,6 +15,18 @@
                 lat: 39.270507,
                 lng: 9.124028,
                 zoom: 18
+            };
+            $scope.defaults = {
+                tileLayer: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+                maxZoom: 19,
+                path: {
+                    weight: 10,
+                    color: '#800000',
+                    opacity: 1
+                }
+            }
+            $scope.geojson = {
+                data: JSON.parse(dataGeoJson)
             };
             promise.then(function (payload) {
                 $scope.laboratory = laboratoriesService.getLaboratoryById.call(payload.data, $stateParams.id);
