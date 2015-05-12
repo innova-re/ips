6caa48b49dfa1624eb4db26eb138d92a8edcf70c
Antonio Pierro <antonio.pierro@gmail.com>
2015-05-12 09:40:36 +0200

    Added a loading spinner

--- www/js/main.js
+++ www/js/main.js
@@ -17,7 +17,8 @@
             angularTranslate: vendorDir + 'angular-translate/angular-translate',
             leaflet: vendorDir + 'leaflet/dist/leaflet',
             // TODO - the angular-leaflet-directive taken with bower does not display the map on android!
-            angularLeafletDirective: 'vendors/angular-leaflet-directive'
+            angularLeafletDirective: 'vendors/angular-leaflet-directive',
+            spin: vendorDir + 'spinjs/spin'
         },
         shim: {
             angular: {
