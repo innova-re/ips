cab4450f6d7f86cdf5be3d20fe65243c7cf44854
Antonio Pierro <antonio.pierro@gmail.com>
2015-05-13 12:06:59 +0200

    Got the real labs coordinates

--- www/js/controllers/map.controller.js
+++ www/js/controllers/map.controller.js
@@ -7,26 +7,26 @@
 
         return ['$scope', '$stateParams', '$http', function ($scope, $stateParams, $http) {
 
-            var promise = $http.get('json/laboratories.json');
+            var promise,
+                getCenter;
 
-            // TODO - take these parameter from the payload
-            $scope.center = {
-                lat: 39.270507,
-                lng: 9.124028,
-                zoom: 18
-            };
             $scope.defaults = {
                 tileLayer: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
                 maxZoom: 20
             };
-
-
-            $scope.geojson = {
-                data: data
+            getCenter = function () {
+                return {
+                    lat: this.lat || 0,
+                    lng: this.lng || 0,
+                    zoom: 18
+                };
             };
-
+            // if missed it raise 'The "center" property is not defined in the main scope'
+            $scope.center = getCenter.call({});
+            promise = $http.get('json/laboratories.json');
             promise.then(function (payload) {
                 $scope.laboratory = laboratoriesService.getLaboratoryById.call(payload.data, $stateParams.id);
+                $scope.center = getCenter.call($scope.laboratory);
             });
         }];
     });
