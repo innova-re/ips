0a43665da53b0199581652d7b74390107ccacde1
Antonio Pierro <antonio.pierro@gmail.com>
2016-01-13 08:57:21 +0100

    Added the notification template

--- www/js/controllers/home.controller.js
+++ www/js/controllers/home.controller.js
@@ -3,9 +3,19 @@
 
     define([
         '../utils/current-location.util',
-        '../utils/services.util'
-    ], function (currentLocationUtil, servicesUtil) {
-        return ['$scope', '$translate', 'geolocation', '$ionicSideMenuDelegate', function ($scope, $translate, geolocation, $ionicSideMenuDelegate) {
+        '../utils/services.util',
+        'text!../../templates/modals/notifications.modal.html'
+    ], function (currentLocationUtil, servicesUtil, notificationsModalTemplate) {
+        return ['$scope', '$translate', 'geolocation', '$ionicSideMenuDelegate', '$ionicModal', function ($scope, $translate, geolocation, $ionicSideMenuDelegate, $ionicModal) {
+
+            // TODO - remove duplicate code; see modal.factory.js -> _setTemplate
+            var _setTemplate = function ($scope, template) {
+                $scope.modal = $ionicModal.fromTemplate(template, {
+                    scope: $scope,
+                    animation: 'slide-in-up'
+                });
+            };
+
             currentLocationUtil($scope, geolocation);
             $scope.lang = $translate.use();
             $scope.toggleLang = function () {
@@ -14,6 +24,10 @@
             };
             $scope.toggleLeft = function () {
                 $ionicSideMenuDelegate.toggleLeft();
+            };
+            $scope.openNotificationsModal = function () {
+                _setTemplate($scope, notificationsModalTemplate);
+                $scope.modal.show();
             };
             $scope.items = servicesUtil.getMenu();
         }];
